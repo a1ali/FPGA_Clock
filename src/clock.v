65e859350802fa9793af609898e31c5e8fbef389
Afaq Ali <39873128+a1ali@users.noreply.github.com>
2021-01-07 15:52:15 -0500

    Uploaded files

---
+++ src/clock.v
@@ -0,0 +1,171 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:32:41 12/30/2020 
+// Design Name: 
+// Module Name:    count 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module count(
+input clk,
+input hours_increment,
+input hours_decrement,
+input mins_increment,
+input mins_decrement,
+output [7:0] SEG,
+output [5:0] ENABLE
+    );
+localparam  one_sec = 11999999; //elbert v2 has a 12MHZ clock 
+reg [23:0] count = 0;
+reg [3:0] seconds = 0;
+reg [3:0] t_secs = 0;
+reg [3:0] minutes = 0;
+reg [3:0] t_mins = 0;
+reg [3:0] hours = 0;
+reg [3:0] t_hours = 0;
+
+wire hour_inc, hour_dec, min_inc, min_dec;
+
+//instances to debounce switches
+debouncer inst1 (.CLK(clk), .switch_input(hours_increment), .trans_dn(hour_inc));
+debouncer inst2 (.CLK(clk), .switch_input(hours_decrement), .trans_dn(hour_dec));
+debouncer inst3 (.CLK(clk), .switch_input(mins_increment), .trans_dn(min_inc));
+debouncer inst4 (.CLK(clk), .switch_input(mins_decrement), .trans_dn(min_dec));
+
+refresh in(.clk(clk), .seconds(seconds), .t_secs(t_secs), .minutes(minutes), .t_mins(t_mins), .hours(hours), .t_hours(t_hours) ,  .SEG(SEG), .ENABLE(ENABLE)); //multiplexing 7-segments instance
+
+always @(posedge clk)
+begin
+
+// Allow switch inputs to increment and decrement hours or minutes
+	if(hour_inc)
+	 begin
+		hours <= hours + 1;
+		if (hours == 9 && t_hours == 0)
+		 begin
+			hours <= 0;
+			t_hours <= 1;
+		 end
+		if (hours == 2 && t_hours == 1)
+		 begin
+			hours <= 1;
+			t_hours <= 0;
+		 end
+	 end
+		 
+	else if (hour_dec)
+	 begin
+		hours <= hours - 1;
+		if (t_hours == 1 && hours == 0)
+		 begin
+			t_hours <= 0;
+			hours <= 9;
+		 end
+		
+		if (t_hours == 0 && hours == 1)
+		 begin
+			t_hours <= 1;
+			hours <= 2;
+		 end
+	 end
+	 
+	 
+	else if (min_inc)
+	 begin
+		minutes <= minutes + 1;
+		if (minutes == 9 && t_mins < 5)
+		 begin
+			minutes <= 0;
+			t_mins <= t_mins + 1;
+		 end
+		if (minutes == 9 && t_mins == 5)
+		 begin
+			minutes <= 0;
+			t_mins <= 0;
+		 end
+	 end
+	 
+	 
+	else if (min_dec)
+	 begin
+		minutes <= minutes - 1;
+		if (minutes == 0 && t_mins > 0)
+		 begin
+			minutes <= 9;
+			t_mins <= t_mins - 1;
+		 end
+		 
+		if (minutes == 0 && t_mins == 0)
+		 begin
+			t_mins <= 5;
+			minutes <= 9;
+		 end
+		
+	 end
+		
+// end of inc/dec
+
+
+// main clock implementation
+	count <= count + 1;
+	if (count == one_sec)
+	begin
+		count <= 0;
+		seconds <= seconds + 1;
+		if (seconds == 9)
+		begin
+			seconds <= 0;
+			t_secs <= t_secs + 1;
+			if (t_secs == 5)
+			 begin
+				t_secs <= 0;
+				minutes <= minutes + 1;
+				if (minutes == 9)
+			    begin
+					minutes <= 0;
+					t_mins <= t_mins + 1;
+					if (t_mins == 5)
+						begin
+						t_mins <= 0;
+						hours <= hours + 1;
+						
+						if (hours == 9 && t_hours == 0)
+							begin
+								hours <= 0;
+								t_hours <= t_hours + 1;	
+							end
+						
+						if (hours == 2 && t_hours == 1)
+							begin
+								t_hours <= 0;
+								hours <= 1;
+							end
+							
+						end
+						
+				 end
+			end
+		end
+	end
+	
+// end of clock implementation
+
+
+end
+
+endmodule
+
+
+
---
+++ src/to_7_seg.v
@@ -0,0 +1,45 @@
+// Module Name:    to_seven_seg 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module to_7_seg(
+		input clk,
+		input  [3:0] count,
+		output reg [7:0] SEG
+    );
+
+//wire [2:0] enable;
+//assign enable = 3'b110;
+
+
+
+always @(posedge clk)
+begin
+	case(count)
+	4'd0: SEG <= 8'b11111100;
+	4'd1: SEG <= 8'b01100000;
+	4'd2: SEG <= 8'b11011010;
+	4'd3: SEG <= 8'b11110010;
+	4'd4: SEG <= 8'b01100110;
+	4'd5: SEG <= 8'b10110110;
+	4'd6: SEG <= 8'b10111110;
+	4'd7: SEG <= 8'b11100000;
+	4'd8: SEG <= 8'b11111110;
+	4'd9: SEG <= 8'b11110110;
+	default: SEG <= 8'b00000000;
+	endcase
+end
+
+endmodule
+
+
+
